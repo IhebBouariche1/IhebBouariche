10e945fe9949624a0a18f8b12db0abce54154e15
Iheb Bouariche <103148161+IhebBouariche1@users.noreply.github.com>
2023-03-29 05:55:43 +0200

    Add files via upload

---
+++ 3-Artificial Intilligent (Deep Reinforcement Learning) - 1/3-Second ordre system Control with Reinforcement learning(DDPG)/SecondOrderSystemControl.m
@@ -0,0 +1,90 @@
+open_system('SecondOrderSystem.slx')
+
+obsInfo = rlNumericSpec([3 1],...
+    'LowerLimit',[-inf -inf 0  ]',...
+    'UpperLimit',[ inf  inf inf]');
+obsInfo.Name = 'observations';
+obsInfo.Description = 'integrated error, error, and measured height';
+numObservations = obsInfo.Dimension(1);
+
+actInfo = rlNumericSpec([1 1]);
+actInfo.Name = 'flow';
+numActions = actInfo.Dimension(1);
+
+env = rlSimulinkEnv('SecondOrderSystem','SecondOrderSystem/RL Agent',...
+    obsInfo,actInfo); %j'ai pas bien comrpris 
+
+
+Ts = 1.0;
+Tf = 200;
+
+rng(0)
+
+statePath = [
+    imageInputLayer([numObservations 1 1],'Normalization','none','Name','State')
+    fullyConnectedLayer(50,'Name','CriticStateFC1')
+    reluLayer('Name','CriticRelu1')
+    fullyConnectedLayer(25,'Name','CriticStateFC2')];
+actionPath = [
+    imageInputLayer([numActions 1 1],'Normalization','none','Name','Action')
+    fullyConnectedLayer(25,'Name','CriticActionFC1')];
+commonPath = [
+    additionLayer(2,'Name','add')
+    reluLayer('Name','CriticCommonRelu')
+    fullyConnectedLayer(1,'Name','CriticOutput')];
+
+criticNetwork = layerGraph();
+criticNetwork = addLayers(criticNetwork,statePath);
+criticNetwork = addLayers(criticNetwork,actionPath);
+criticNetwork = addLayers(criticNetwork,commonPath);
+criticNetwork = connectLayers(criticNetwork,'CriticStateFC2','add/in1');
+criticNetwork = connectLayers(criticNetwork,'CriticActionFC1','add/in2');
+
+figure
+plot(criticNetwork)
+
+criticOpts = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1);
+
+critic = rlRepresentation(criticNetwork,obsInfo,actInfo,'Observation',{'State'},'Action',{'Action'},criticOpts);
+
+actorNetwork = [
+    imageInputLayer([numObservations 1 1],'Normalization','none','Name','State')
+    fullyConnectedLayer(3, 'Name','actorFC')
+    tanhLayer('Name','actorTanh')
+    fullyConnectedLayer(numActions,'Name','Action')
+    ];
+
+actorOptions = rlRepresentationOptions('LearnRate',1e-04,'GradientThreshold',1);
+
+actor = rlRepresentation(actorNetwork,obsInfo,actInfo,'Observation',{'State'},'Action',{'Action'},actorOptions);
+
+agentOpts = rlDDPGAgentOptions(...
+    'SampleTime',Ts,...
+    'TargetSmoothFactor',1e-3,...
+    'DiscountFactor',1.0, ...
+    'MiniBatchSize',64, ...
+    'ExperienceBufferLength',1e6); 
+agentOpts.NoiseOptions.Variance = 0.3;
+agentOpts.NoiseOptions.VarianceDecayRate = 1e-5;
+
+agent = rlDDPGAgent(actor,critic,agentOpts);
+
+maxepisodes = 500;
+maxsteps = ceil(Tf/Ts);
+trainOpts = rlTrainingOptions(...
+    'MaxEpisodes',1000,...
+    'MaxStepsPerEpisode',500,...
+    'ScoreAveragingWindowLength',10,...
+    'Verbose',false,...
+    'Plots','training-progress',...
+    'StopTrainingCriteria','AverageReward',...
+    'StopTrainingValue',10000,...
+    'SaveAgentCriteria','EpisodeReward',...
+    'SaveAgentValue',10000);
+
+trainingStats = train(agent,env,trainOpts);
+
+simOpts = rlSimulationOptions('MaxSteps',maxsteps,'StopOnError','on');
+experiences = sim(env,agent,simOpts);
+
+
