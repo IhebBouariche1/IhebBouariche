8dbae66aef02db3cd47c8ecd638632b89ac0dbf6
Iheb Bouariche <103148161+IhebBouariche1@users.noreply.github.com>
2023-07-19 03:10:19 +0200

    Add files via upload

---
+++ End Of Studies Research' Project/Ingeniorat/Trajectory_optimization and Control with D-RL/Training_precision.m
@@ -0,0 +1,19 @@
+clear all 
+clc 
+
+load('DATA')
+
+mdl = 'iiwa14_simulink_reaching1';  
+blk = [mdl, '/RL Agent'];
+env = rlSimulinkEnv(mdl,blk,obsInfo,actInfo);
+
+% agentOptions.NoiseOptions.MeanAttractionConstant = 1e-6;
+agentOptions.NoiseOptions.StandardDeviation = 0.1*sqrt(Ts);
+agentOptions.NoiseOptions.StandardDeviationDecayRate = 1e-4;
+
+trainingOptions.MaxEpisodes = 10000;
+trainingOptions.SaveAgentValue = 1800;
+trainingOptions.MaxStepsPerEpisode = 199;
+trainingOptions.ScoreAveragingWindowLength = 100;
+trainingOptions.StopTrainingValue  = 1800;  
+trainingStats = train(agent,env,trainingOptions);
